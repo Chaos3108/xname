1f34a6d45a8c8ef6a3f1c49576b8f18728322fde
yuva kishore <yuvakishorekolli@gmail.com>
2025-02-08 10:24:13 +0530

    implementd updated according to test cases

--- src/App.js
+++ src/App.js
@@ -6,44 +6,50 @@ function App() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLasttName] = useState("");
   const [isSubmitClicked, setIsSubmitClicked] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmitClicked(true);
+  };
 
   return (
     <div>
       <div>
         <h2>Full Name Display</h2>
-        <label>
-          First Name :
-          <input
-            type="text"
-            onChange={(e) => {
-              setFirstName(e.target.value);
-            }}
-            value={firstName}
-            placeholder="Enter FirstName"
-          />
-        </label>
       </div>
-      <label>
-        Last Name :
-        <input
-          type="text"
-          onChange={(e) => {
-            setLasttName(e.target.value);
-          }}
-          value={lastName}
-          placeholder="Enter LastName"
-        />
-      </label>
-      <div>
-        <button
-          name="submit"
-          onClick={() => {
-            setIsSubmitClicked(true);
-          }}
-        >
-          submit
-        </button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <>
+          <label>
+            First Name :
+            <input
+              required
+              type="text"
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+              value={firstName}
+              placeholder="Enter FirstName"
+            />
+          </label>
+          <label>
+            Last Name :
+            <input
+              type="text"
+              onChange={(e) => {
+                setLasttName(e.target.value);
+              }}
+              required
+              value={lastName}
+              placeholder="Enter LastName"
+            />
+          </label>
+        </>
+        <div>
+          <button name="submit" type="submit">
+            submit
+          </button>
+        </div>
+      </form>
+
       {isSubmitClicked ? (
         <>
           {" "}
